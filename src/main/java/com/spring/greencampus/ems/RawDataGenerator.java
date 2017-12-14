d78a918b6e023877c1a09da2372907bee295b4ac
xujingyapatrick <xujingyastan@gmail.com>
2017-12-14 11:02:07 -0800

    with gitignore

--- src/main/java/com/spring/greencampus/ems/RawDataGenerator.java
+++ src/main/java/com/spring/greencampus/ems/RawDataGenerator.java
@@ -1,16 +1,8 @@
 package com.spring.greencampus.ems;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KStreamBuilder;
-import org.apache.kafka.streams.kstream.KTable;
 
-import java.util.Arrays;
 import java.util.Properties;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
@@ -39,7 +31,7 @@ public class RawDataGenerator {
     }
     public static void main(String[] args) throws InterruptedException {
         Properties config = new Properties();
-        config.put("bootstrap.servers", "127.0.0.1:2097");
+        config.put("bootstrap.servers", "127.0.0.1:7092");
         config.put("client.id", "RawDataProducer");
         config.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
         config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
@@ -49,7 +41,7 @@ public class RawDataGenerator {
             Thread.sleep(1000);
             try{
                 producer.send(new ProducerRecord<>("raw-data-input", getSmartmeterId(), getRawData())).get();
-                System.out.println("Sent Message Success");
+//                System.out.println("Sent Message Success");
             }
             catch (InterruptedException|ExecutionException e){
                 e.printStackTrace();
@@ -59,38 +51,7 @@ public class RawDataGenerator {
             }
         }
         System.out.println("This is the end of sending messages.");
-//
-//        KStreamBuilder builder = new KStreamBuilder();
-//        // 1 - stream from Kafka
-//        KStream<Integer, String> kv =
-//
-//        KStream<String, String> textLines = builder.stream("raw-data-input");
-//        KTable<String, Long> wordCounts = textLines
-//                // 2 - map values to lowercase
-//                .mapValues(textLine -> textLine.toLowerCase())
-//                // can be alternatively written as:
-//                // .mapValues(String::toLowerCase)
-//                // 3 - flatmap values split by space
-//                .flatMapValues(textLine -> Arrays.asList(textLine.split("\\W+")))
-//                // 4 - select key to apply a key (we discard the old key)
-//                .selectKey((key, word) -> word)
-//                // 5 - group by key before aggregation
-//                .groupByKey()
-//                // 6 - count occurences
-//                .count("Counts");
-//
-//        // 7 - to in order to write the results back to kafka
-//        wordCounts.to(Serdes.String(), Serdes.Long(), "word-count-output");
-//
-//        KafkaStreams streams = new KafkaStreams(builder, config);
-//        streams.start();
-//
-//        // print the topology
-//        System.out.println(streams.toString());
-//
-//        // shutdown hook to correctly close the streams application
-//        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
-//        System.out.println("This is a test");
+
     }
 
 }
